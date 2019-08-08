e5e7845d9a02b9d8301c668ad5475b8ab698535f
sergei <sergei@mail.ru>
2019-08-08 16:50:27 +0300

    #1_master First template

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require("gulp");
+const scss = require("gulp-sass");
+const clean = require("gulp-clean");
+const browserSync = require("browser-sync").create();
+
+function buildScss (){
+    return gulp.src('./develop/scss/**/*.scss')
+            .pipe(scss())
+            .pipe(gulp.dest('./build/css'))
+            .pipe(browserSync.stream())
+}
+
+function buildHtml (){
+    return gulp.src('./develop/*.html')
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream())
+}
+
+function cleanBuild(){
+    return gulp.src('build/*')
+            .pipe(clean());
+}
+
+function browser(done){
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        }
+    });
+    done();
+    // gulp.watch('./develop/*.html', buildHtml);
+    // gulp.watch('./develop/scss/*.scss', buildScss);
+  
+}
+
+function watchScss(){
+   return gulp.watch('./develop/scss/*.scss', gulp.series(buildScss, browserSync.reload));
+}
+
+gulp.task('scss', buildScss);
+gulp.task('buildHtml', buildHtml);
+gulp.task('clean', cleanBuild);
+gulp.task('watch', watchScss);
+
+
+gulp.task('production', gulp.series('clean', gulp.series(cleanBuild, buildHtml, buildScss, browser)));
+
+                   
