f3ebbd8494a9b8e38353d6a2039360e915c161a6
sergei <sergei@mail.ru>
2019-08-12 16:57:32 +0300

    #4_master some changes in scss file and add about.txt

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require("gulp");
 const scss = require("gulp-sass");
 const clean = require("gulp-clean");
-// const browserSync = require("browser-sync").create();
 const browserSync = require("browser-sync").create();
 
 function buildScss (){
@@ -9,44 +8,30 @@ function buildScss (){
             .pipe(scss())
             .pipe(gulp.dest('./build/css'))
             .pipe(browserSync.stream())
-}
+};
 
 
 // и browserSync.stream() срабатывает и .pipe(browserSync.reload({stream: true}))
 function buildHtml (){
     return gulp.src('./develop/*.html')
     .pipe(gulp.dest('./build'))
-    // .pipe(browserSync.reload({
-    //     stream: true
-    //   }))
     .pipe(browserSync.stream())
-}
+};
 
 function cleanBuild(){
     return gulp.src('build/*')
             .pipe(clean());
-}
+};
 
-    // gulp.watch('./develop/*.html', buildHtml);
-    // gulp.watch('./develop/scss/*.scss', buildScss);
-
-
-// function watchScss(){
-//    return gulp.watch('./develop/scss/*.scss', gulp.series(buildScss, browserSync.reload));
-// }
 
 function watch(){
     gulp.watch('./develop/*.html', buildHtml);
     gulp.watch('./develop/scss/*.scss', buildScss);
-}
+};
 
 gulp.task('scss', buildScss);
 gulp.task('buildHtml', buildHtml);
 gulp.task('clean', cleanBuild);
-// gulp.task('watch', watchScss);
-
-
-
 
 // запускаем browser chrom и указываем папку, в которой отслеживаемый index.html.
 // но автоматической перезагрузки браузера не происходит.
